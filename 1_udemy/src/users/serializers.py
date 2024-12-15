d035ffe22c2e6db1ff1ef7f1d5d3f0e4829efa50
kaditya67 <322adityakumar0004@dbit.in>
2024-12-15 18:47:35 +0530

    Profile added

--- 1_udemy/src/users/serializers.py
+++ 1_udemy/src/users/serializers.py
@@ -1,10 +1,33 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import Profile
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ['id', 'user', 'image']
+
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=False)
     old_password = serializers.CharField(write_only=True, required=False)
     username = serializers.CharField(read_only=True)
+    profile = ProfileSerializer(read_only=True)
+
+    def validate(self, data):
+        request_method = self.context.get('request').method
+        password = data.get('password', None)   
+        if request_method == 'POST':
+            if not password:
+                raise serializers.ValidationError({'password': ['This field is required.']})
+        elif request_method == 'PUT' or request_method == 'PATCH':
+            old_password = data.get('old_password', None)
+            if not password:
+                raise serializers.ValidationError({'password': ['Password is required.']})
+            elif not old_password:
+                raise serializers.ValidationError({'old_password': ['old_password is required.']})
+        return data
+        
 
     def create(self, validated_data):
         # Remove `old_password` if it exists
@@ -52,4 +75,5 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'password', 'old_password']
+        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'password', 'old_password','profile']
+
--- 1_udemy/src/users/viewsets.py
+++ 1_udemy/src/users/viewsets.py
@@ -1,10 +1,16 @@
 from rest_framework import viewsets
-from .serializers import UserSerializer
 from django.contrib.auth.models import User
-
+from .models import Profile
+from .serializers import UserSerializer, ProfileSerializer
 from .permissions import IsUserOwnerOrGetAndPostOnly
 
 class UserViewSet(viewsets.ModelViewSet):
     permission_classes = [IsUserOwnerOrGetAndPostOnly]
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    lookup_field = 'id' 
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    lookup_field = 'id'
